ffe8733c12050e57d4fb412fb12c5a60535c684d
slavaheroes <shen9910@gmail.com>
2022-06-11 14:05:14 +0600

    hw3 finish

--- hw3/homework.py
+++ hw3/homework.py
@@ -4,53 +4,87 @@ from sklearn.feature_extraction import DictVectorizer
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
+from prefect import flow, task, get_run_logger
+from prefect.task_runners import SequentialTaskRunner
+from datetime import date, datetime
+from prefect.deployments import DeploymentSpec
+from prefect.orion.schemas.schedules import CronSchedule
+from prefect.flow_runners import SubprocessFlowRunner
+import os
+import pickle
+
+
+@task
+def get_paths(date):
+    date = date.today() if date == None else datetime.strptime(date, "%Y-%m-%d")
+
+    month = date.month
+
+    train_path = f"./data/fhv_tripdata_{date.year}-{(date.month - 2):02d}.parquet"
+    val_path = f"./data/fhv_tripdata_{date.year}-{(date.month - 1):02d}.parquet"
+
+    return train_path, val_path, date
+
+
+@task
 def read_data(path):
     df = pd.read_parquet(path)
     return df
 
+
+@task
 def prepare_features(df, categorical, train=True):
+    logger = get_run_logger()
     df['duration'] = df.dropOff_datetime - df.pickup_datetime
     df['duration'] = df.duration.dt.total_seconds() / 60
     df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
 
     mean_duration = df.duration.mean()
     if train:
-        print(f"The mean duration of training is {mean_duration}")
+        logger.info(f"The mean duration of training is {mean_duration}")
     else:
-        print(f"The mean duration of validation is {mean_duration}")
+        logger.info(f"The mean duration of validation is {mean_duration}")
     
     df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
     return df
 
+
+@task
 def train_model(df, categorical):
+    logger = get_run_logger()
 
     train_dicts = df[categorical].to_dict(orient='records')
     dv = DictVectorizer()
     X_train = dv.fit_transform(train_dicts) 
     y_train = df.duration.values
 
-    print(f"The shape of X_train is {X_train.shape}")
-    print(f"The DictVectorizer has {len(dv.feature_names_)} features")
+    logger.info(f"The shape of X_train is {X_train.shape}")
+    logger.info(f"The DictVectorizer has {len(dv.feature_names_)} features")
 
     lr = LinearRegression()
     lr.fit(X_train, y_train)
     y_pred = lr.predict(X_train)
     mse = mean_squared_error(y_train, y_pred, squared=False)
-    print(f"The MSE of training is: {mse}")
+    logger.info(f"The MSE of training is: {mse}")
     return lr, dv
 
+
+@task
 def run_model(df, categorical, dv, lr):
+    logger = get_run_logger()
     val_dicts = df[categorical].to_dict(orient='records')
     X_val = dv.transform(val_dicts) 
     y_pred = lr.predict(X_val)
     y_val = df.duration.values
 
     mse = mean_squared_error(y_val, y_pred, squared=False)
-    print(f"The MSE of validation is: {mse}")
+    logger.info(f"The MSE of validation is: {mse}")
     return
 
-def main(train_path: str = './data/fhv_tripdata_2021-01.parquet', 
-           val_path: str = './data/fhv_tripdata_2021-02.parquet'):
+
+@flow(task_runner = SequentialTaskRunner())
+def main(date=None):
+    train_path, val_path, date = get_paths(date).result()
 
     categorical = ['PUlocationID', 'DOlocationID']
 
@@ -61,7 +95,26 @@ def main(train_path: str = './data/fhv_tripdata_2021-01.parquet',
     df_val_processed = prepare_features(df_val, categorical, False)
 
     # train the model
-    lr, dv = train_model(df_train_processed, categorical)
+    lr, dv = train_model(df_train_processed, categorical).result()
     run_model(df_val_processed, categorical, dv, lr)
 
-main()
+    os.makedirs("models/", exist_ok = True)
+
+    with open(f'models/model-{date.year}-{date.month:02d}-{date.day}.bin', "wb") as f:
+        pickle.dump(lr, f)
+
+    with open(f'models/dv-{date.year}-{date.month:02d}-{date.day}.bin', "wb") as f:
+        pickle.dump(dv, f)
+
+
+
+
+DeploymentSpec(
+    flow=main,
+    name="model_training",
+    schedule=CronSchedule(cron="0 9 15 * *"),
+    flow_runner=SubprocessFlowRunner(),
+    tags=["ml"]
+)
+
+# main(date="2021-08-15")
